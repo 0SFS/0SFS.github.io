fbc44c2355b910a1fc2377b6ae32b200d2e02523
Felipegalind0 <Felipegalind0@outlook.com>
2026-09-10 11:59:11 -0500

    Updates

--- assets/volumetricLightingBlendVolume.fragment-CNNePrSO.js
+++ assets/volumetricLightingBlendVolume.fragment-CNNePrSO.js
@@ -1,4 +1,4 @@
-import{ms as e}from"./resolveMapRuntimeConfig-CQr1gEiV.js";var t=`volumetricLightingBlendVolumePixelShader`,n=`varying vec2 vUV;uniform sampler2D textureSampler;uniform sampler2D depthSampler;uniform mat4 invProjection;uniform vec2 outputTextureSize;
+import{ps as e}from"./resolveMapRuntimeConfig-DHBvCMR0.js";var t=`volumetricLightingBlendVolumePixelShader`,n=`varying vec2 vUV;uniform sampler2D textureSampler;uniform sampler2D depthSampler;uniform mat4 invProjection;uniform vec2 outputTextureSize;
 #ifdef USE_EXTINCTION
 uniform vec3 extinction;
 #endif
