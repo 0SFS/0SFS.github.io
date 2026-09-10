7fed9ea045835270b740520c886706f7cb567481
Felipegalind0 <Felipegalind0@outlook.com>
2026-09-10 01:25:04 -0500

    Updates

--- assets/selection.fragment-CphIkeak.js
+++ assets/selection.fragment-CphIkeak.js
@@ -1,4 +1,4 @@
-import{ms as e}from"./resolveMapRuntimeConfig-SrS2BVQR.js";var t=`selectionPixelShader`,n=`#ifdef INSTANCES
+import{ms as e}from"./resolveMapRuntimeConfig-DMxd6Xou.js";var t=`selectionPixelShader`,n=`#ifdef INSTANCES
 flat varying float vSelectionId;
 #else
 uniform float selectionId;
--- assets/volumetricLightingBlendVolume.fragment-DcsBSJAZ.js
+++ assets/volumetricLightingBlendVolume.fragment-DcsBSJAZ.js
@@ -1,4 +1,4 @@
-import{ms as e}from"./resolveMapRuntimeConfig-SrS2BVQR.js";var t=`volumetricLightingBlendVolumePixelShader`,n=`varying vUV: vec2f;var textureSamplerSampler: sampler;var textureSampler: texture_2d<f32>;var depthSampler: texture_2d<f32>;uniform invProjection: mat4x4<f32>;uniform outputTextureSize: vec2f;
+import{ms as e}from"./resolveMapRuntimeConfig-DMxd6Xou.js";var t=`volumetricLightingBlendVolumePixelShader`,n=`varying vUV: vec2f;var textureSamplerSampler: sampler;var textureSampler: texture_2d<f32>;var depthSampler: texture_2d<f32>;uniform invProjection: mat4x4<f32>;uniform outputTextureSize: vec2f;
 #ifdef USE_EXTINCTION
 uniform extinction: vec3f;
 #endif
