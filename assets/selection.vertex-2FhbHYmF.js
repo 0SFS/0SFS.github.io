c0dbd20cb830a7a71f5acaeb8c005be0c47245a1
Felipegalind0 <Felipegalind0@outlook.com>
2026-09-09 22:59:44 -0500

    Updates

--- assets/selection.vertex-2FhbHYmF.js
+++ assets/selection.vertex-2FhbHYmF.js
@@ -1,4 +1,4 @@
-import{ms as e}from"./resolveMapRuntimeConfig-BODiAhoi.js";var t=`selectionVertexShader`,n=`attribute position: vec3f;
+import{ms as e}from"./resolveMapRuntimeConfig-BVYC4xBU.js";var t=`selectionVertexShader`,n=`attribute position: vec3f;
 #ifdef INSTANCES
 attribute instanceSelectionId: f32;
 #endif
