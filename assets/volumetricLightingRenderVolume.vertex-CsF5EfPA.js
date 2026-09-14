e3efa8cdec96e2764bba63f3259bf43111612fbf
Felipegalind0 <Felipegalind0@outlook.com>
2026-09-13 23:31:46 -0500

    Updates

---
+++ assets/volumetricLightingRenderVolume.vertex-CsF5EfPA.js
@@ -0,0 +1,4 @@
+import{Ss as e}from"./resolveMapRuntimeConfig-DD6xMBdn.js";var t=`volumetricLightingRenderVolumeVertexShader`,n=`#include<__decl__sceneVertex>
+#include<__decl__meshVertex>
+attribute vec3 position;varying vec4 vWorldPos;void main(void) {vec4 worldPos=world*vec4(position,1.0);vWorldPos=worldPos;gl_Position=viewProjection*worldPos;}
+`;e.ShadersStore[t]||(e.ShadersStore[t]=n);var r={name:t,shader:n};export{r as volumetricLightingRenderVolumeVertexShader};
