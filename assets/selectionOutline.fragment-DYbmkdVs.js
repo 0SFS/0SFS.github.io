5d26871de150866781a604aca7f5761479824fb7
Felipegalind0 <Felipegalind0@outlook.com>
2026-09-14 05:11:45 -0500

    Updates

--- assets/selectionOutline.fragment-DYbmkdVs.js
+++ assets/selectionOutline.fragment-DYbmkdVs.js
@@ -1,4 +1,4 @@
-import{Ss as e}from"./resolveMapRuntimeConfig-DD6xMBdn.js";var t=`selectionOutlinePixelShader`,n=`uniform sampler2D maskSampler;uniform sampler2D depthSampler;varying vec2 vUV;uniform vec2 screenSize;uniform vec3 outlineColor;uniform float outlineThickness;uniform float occlusionStrength;uniform float occlusionThreshold;
+import{Ss as e}from"./resolveMapRuntimeConfig-BfvFW69J.js";var t=`selectionOutlinePixelShader`,n=`uniform sampler2D maskSampler;uniform sampler2D depthSampler;varying vec2 vUV;uniform vec2 screenSize;uniform vec3 outlineColor;uniform float outlineThickness;uniform float occlusionStrength;uniform float occlusionThreshold;
 #define CUSTOM_FRAGMENT_DEFINITIONS
 void main(void) {
 #define CUSTOM_FRAGMENT_MAIN_BEGIN
